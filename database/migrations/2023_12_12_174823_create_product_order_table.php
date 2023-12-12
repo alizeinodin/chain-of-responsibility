93b37726f096a241766569ca17500ef85e508e87
alizeinodin <alizeinodin79@gmail.com>
2023-12-12 22:06:44 +0330

    Change table name

--- database/migrations/2023_12_12_174823_create_product_order_table.php
+++ database/migrations/2023_12_12_174823_create_product_order_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('product_order', function (Blueprint $table) {
+        Schema::create('order_product', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(\App\Models\Product::class);
             $table->foreignIdFor(\App\Models\Order::class);
