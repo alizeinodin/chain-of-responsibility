d1e71aefe1ce53b58a6553be89bcd2f2479f94c9
alizeinodin <alizeinodin79@gmail.com>
2023-12-12 21:25:59 +0330

    Add one-to-many relationship between Order and Transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Http\Enum\TransactionStatus;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
@@ -15,4 +16,9 @@ class Transaction extends Model
     protected $casts = [
         'status' => TransactionStatus::class,
     ];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
--- database/migrations/2023_12_12_162128_create_transactions_table.php
+++ database/migrations/2023_12_12_162128_create_transactions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
             $table->id();
             $table->unsignedBigInteger('amount');
             $table->string('status');
+            $table->foreignIdFor(\App\Models\Order::class);
             $table->timestamps();
         });
     }
