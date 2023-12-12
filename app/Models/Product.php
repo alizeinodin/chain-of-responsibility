51abe0eb582287c7b4d40d4a01a1bb0e7771644b
alizeinodin <alizeinodin79@gmail.com>
2023-12-12 21:21:32 +0330

    Add many-to-many relationship between Product and Order

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
@@ -12,4 +13,9 @@ class Product extends Model
         'title',
         'description',
     ];
+
+    public function orders(): BelongsToMany
+    {
+        return $this->belongsToMany(Order::class);
+    }
 }
