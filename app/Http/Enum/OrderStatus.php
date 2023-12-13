cdbcff494a7524455660f9825df0bdd009b759e0
alizeinodin <alizeinodin79@gmail.com>
2023-12-13 20:23:45 +0330

    Add new status to OrderStatus

--- app/Http/Enum/OrderStatus.php
+++ app/Http/Enum/OrderStatus.php
@@ -5,6 +5,7 @@ namespace App\Http\Enum;
 enum OrderStatus: string
 {
     const PENDING = 'pending';
+    const AWAITING_PAYMENT = 'awaiting payment';
     const PROCESSING = 'processing';
     const CANCELED = 'canceled';
     const COMPLETED = 'completed';
