1301351ed09b53c7240ec9b2b43634e6611315f0
alizeinodin <alizeinodin79@gmail.com>
2023-12-15 20:56:49 +0330

    Add CreditChecking middleware

---
+++ app/Http/Middleware/CreditChecking.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CreditChecking
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(Request): (Response) $next
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!($request->user()->credit >= $request->product->cost)) {
+            return response()
+                ->json([
+                    'message' => 'Not enough credit, please charge it!'
+                ], Response::HTTP_FORBIDDEN);
+        }
+        return $next($request);
+    }
+}
